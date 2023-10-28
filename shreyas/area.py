60c86c3e1982f88a5b137f08a882bcd0ad938031
KILLinefficiency <shreyas.sable2166@gmail.com>
2023-10-28 19:29:03 +0530

    added a program for area

---
+++ shreyas/area.py
@@ -0,0 +1,6 @@
+r = float(input("Enter the radius: "))
+print("Area is", 3.14 * r * r)
+
+b = float(input("Enter the base of the triangle: "))
+h = float(input("Enter the height of the triangle: "))
+print("Area is", 0.5 * b * h)
